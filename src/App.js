b9d7ad2aa63186b6789863b5c7e23c46ac799f5d
KasimovAslbek <KasimovAslbek1994@mail.ru>
2023-07-27 18:34:13 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import Main from "./components/Main";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <Main/>
+        <p className="text-red-500 ">
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
